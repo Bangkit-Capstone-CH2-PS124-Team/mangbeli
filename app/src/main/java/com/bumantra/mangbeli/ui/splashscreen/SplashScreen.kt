45753d989e00d0564401ee3d1df838b0c5018b97
FadlyRamdhani4503 <91235752+FadlyRamdhani4503@users.noreply.github.com>
2023-11-23 17:13:07 +0700

    Create Splash Screen along with the delay

---
+++ app/src/main/java/com/bumantra/mangbeli/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,39 @@
+package com.bumantra.mangbeli.ui.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bumantra.mangbeli.databinding.ActivitySplashScreenBinding
+import com.bumantra.mangbeli.ui.MainActivity
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val splashDelay: Long = 1000
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (!isFinishing) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, splashDelay)
+    }
+
+    override fun onDestroy() {
+        Handler(Looper.getMainLooper()).removeCallbacksAndMessages(null)
+        super.onDestroy()
+    }
+}
