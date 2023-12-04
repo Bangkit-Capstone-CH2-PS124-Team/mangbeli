f65987295d81315ae97d660bcf059b6efea117ac
Raafi Hilmi <raafihilmi90@gmail.com>
2023-12-04 17:33:39 +0700

    Change the logo and fix dark mode in the splashscreen

--- app/src/main/java/com/bumantra/mangbeli/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/bumantra/mangbeli/ui/splash/SplashScreen.kt
@@ -7,11 +7,17 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
+import com.bumantra.mangbeli.data.local.pref.SettingsPref
+import com.bumantra.mangbeli.data.local.pref.dataStore
 import com.bumantra.mangbeli.databinding.ActivitySplashScreenBinding
 import com.bumantra.mangbeli.ui.MenuActivity
 import com.bumantra.mangbeli.ui.ViewModelFactory
 import com.bumantra.mangbeli.ui.home.HomeActivity
 import com.bumantra.mangbeli.ui.home.HomeViewModel
+import com.bumantra.mygithubusers.ui.settings.SettingViewModel
+import com.bumantra.mygithubusers.ui.settings.SettingViewModelFactory
 
 
 @SuppressLint("CustomSplashScreen")
@@ -27,6 +33,18 @@ class SplashScreen : AppCompatActivity() {
         supportActionBar?.hide()
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val pref = SettingsPref.getInstance(applicationContext.dataStore)
+        val settingViewModel = ViewModelProvider(this, SettingViewModelFactory(pref))[SettingViewModel::class.java]
+
+        settingViewModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
         viewModel.getSession().observe(this){ user ->
             if (user.isLogin){
                 Handler(Looper.getMainLooper()).postDelayed({
